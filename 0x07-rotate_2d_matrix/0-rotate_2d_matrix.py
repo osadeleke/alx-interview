29aacdb2feffc2438bfda011e223d09f0f47eda8
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-31 05:18:34 +0100

    config and file

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+rotate a 2d matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate 2d matrix
+    """
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
