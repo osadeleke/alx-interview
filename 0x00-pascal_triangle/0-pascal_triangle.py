74b8d0d99e965b4ceb701d2bd43f4dfa5b494014
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-14 18:29:44 +0100

    Fix: line length, remove import and fix shebang

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Python pascal triangle module
 """
-from typing import List
 
 
-def pascal_triangle(n: int) -> List:
+def pascal_triangle(n):
     """
     module for pascal triangle
     """
     if n > 0:
-        triangle: List[List[int]] = [[1]]
+        triangle = [[1]]
         if n > 1:
             for i in range(1, n):
                 temp_triangle = [1]
                 for j in range(1, i):
-                    temp_triangle.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+                    temp_triangle.append(triangle[i - 1][j - 1] + triangle[i - 1][j])  # noqa
                 temp_triangle.append(1)
                 triangle.append(temp_triangle)
         return triangle
